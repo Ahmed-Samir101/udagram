5868504043e038704b6dc02ba782d55b11cfa425
Ahmed Samir <ahmedhtml.art@gmail.com>
2023-03-11 14:24:36 +0200

    added the rest of the app

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=access8080-aws
+export POSTGRES_HOST=database-1.cnktqd2bxnpk.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::myawsbucket5775
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=mysecretstring
+export URL=http://localhost:8100
